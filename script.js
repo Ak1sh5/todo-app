582246b11e1e3d8dff84e63fe13f1037ea3d5597
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2023-10-19 18:09:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const inputText=document.getElementById("newTask");
 const resultText=document.getElementById("placeForNewTask");
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(function (taskText) {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox">
+        <label>${taskText}</label>
+        <button class="delete-button">Delete</button>
+        <button class="edit-button">Edit</button>`;
+        resultText.appendChild(li);
+    });
+}
+function saveTasks() {
+    const tasks = Array.from(resultText.querySelectorAll("li label")).map(label => label.textContent);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 function addElement(){
     const text = inputText.value.trim();
     if(text!=""){
@@ -10,16 +27,25 @@ function addElement(){
         <button class="edit-button" >Edit</button>`;
         resultText.appendChild(li);
         inputText.value="";
-     
+        saveTasks();
     }    
     
 }
+inputText.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        addElement();
+    }
+});
+
+
 function clearElement(){
     const completedTasks = resultText.querySelectorAll("li input[type='checkbox']:checked");
         completedTasks.forEach(function (task) {
             const li = task.parentElement;
             li.remove();
+            
         });
+        saveTasks();
 
 }
 
@@ -27,6 +53,7 @@ resultText.addEventListener("click", function (event) {
     if (event.target.classList.contains("delete-button")) {
         const listItem = event.target.parentElement;
         resultText.removeChild(listItem);
+        saveTasks();
     }
     else if (event.target.classList.contains("edit-button")) {
         // Handle edit action
@@ -36,5 +63,9 @@ resultText.addEventListener("click", function (event) {
         if (newText !== null) {
             label.textContent = newText;
         }
+        saveTasks();
     }
+    
 });
+
+loadTasks();
